0b7956aff1bc2854bf81acc30a96ab31830249ef
Trang <pctrangmeo161@gmail.com>
2023-12-27 04:20:26 +0900

    first ex

---
+++ ex1/index.js
@@ -0,0 +1,18 @@
+//Bài 1: Hello word
+console.log("Hello World!");
+
+//Bài 2: Khai báo biến
+const helloWorld = "Hello World!!";
+console.log(helloWorld);
+
+//Bài 3: Biểu thức
+const hello = "Hello";
+const world = "World!!!";
+const hello_world = hello + " " + world;
+console.log(hello_world);
+
+//Bài 4: Hàm số 
+function helloWorldFunction(string) {
+    console.log(string);
+};
+helloWorldFunction(hello_world);
